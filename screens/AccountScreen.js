60a252d3a89d400ae84100f29d8cb8a73fb6c44b
rita_tony <rita_tony@hotmail.com>
2022-02-12 17:43:52 +0800

    first commit

---
+++ screens/AccountScreen.js
@@ -0,0 +1,73 @@
+import React, {useState, useEffect} from "react";
+import { ActivityIndicator, Button, StyleSheet, Text, View } from "react-native";
+import { commonStyles } from "../styles/commonStyles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const API = "http://ritatony.pythonanywhere.com/";
+const API_WHOAMI = "/whoami";
+
+
+export default function AccountScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+
+  async function getUsername() {
+    console.log("---- Getting User Name ----");
+    const token = await AsyncStorage.getItem("token");
+    console.log(`Token is ${token}`);
+
+    try {
+      const response = await axios.get(API + API_WHOAMI, {
+        headers: { Authorization: `JWT ${token}` },
+      });
+
+      console.log("Got user name!");
+      setUsername(response.data.username);
+      console.log(response);
+    }
+    catch (error) {
+      console.log("Error getting user name");
+      if (error.response) {
+        console.log(error.response.data);
+
+        if (error.response.data.status_code === 400) {
+          signOut();
+        }
+      }
+      else {
+        console.log(error);
+      }
+    }   
+  }
+
+
+  useEffect(() => {
+    console.log("Setting up nav listener");
+    //Check for when we come back to this screen
+    const removeListener = navigation.addListener("focus", () => {
+      console.log("Running nav listener");
+      setUsername(<ActivityIndicator />);
+      getUsername();
+    });
+
+    getUsername();
+
+    return removeListener;
+  },[]
+  );
+
+  function signOut() {
+    AsyncStorage.removeItem("token");
+    navigation.navigate("SignIn");
+  }
+
+  return (
+    <View style={commonStyles.container}>
+      <Text>Account Screen</Text>
+      <Text>{username}</Text>
+      <Button title="Sign out" onPress={signOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
